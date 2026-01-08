61fbc7f55f0e0482d5b1f3a368e4e3fe396a2dda
Yuwol <juneyoung.jeong66@outlook.com>
2026-01-09 05:28:35 +1000

    Refactor Program class into GameSettings and Game
    Refactored the massive Program class and the Main method into GameSettings and Game class; GameSettings handles the inital settings and party creations, and Game handles the gameplay logic and the display.

---
+++ src/ConsoleRpg/Game.cs
@@ -0,0 +1,131 @@
+﻿using ConsoleRpg.Characters;
+using ConsoleRpg.Gears;
+using ConsoleRpg.Controllers;
+using ConsoleRpg.Parties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpg
+{
+    public class Game
+    {
+        private GameSettings gameSettings;
+        public Game(GameSettings gameSettings)
+        {
+            this.gameSettings = gameSettings;
+        }
+
+        public void RunGame()
+        {
+            while (true)
+            {
+                List<Character> originalCharacters = gameSettings.CharacterManager.GetCharacters();
+                for (int i = originalCharacters.Count - 1; i >= 0; i--)
+                {
+                    Console.ResetColor();
+                    Character character = originalCharacters[i];
+                    if (character.IsDead()) continue;
+                    var gameEnded = gameSettings.CharacterManager.CheckWinningSide();
+                    if (gameEnded.ended == true)
+                    {
+                        if (gameEnded.winningSide == Side.Enemy)
+                        {
+                            Console.WriteLine("The heroes lost and Satan's forces have prevailed... The world will end now.");
+                        }
+                        else
+                        {
+                            if (gameSettings.CharacterManager.Stage == 0)
+                            {
+                                Console.WriteLine("The heroes have cleared the first floor. They move on to the next floor.");
+                                ProgressStage(gameSettings.CharacterManager, gameSettings.AllyParty, gameSettings.EnemyPartyTwo);
+                                break;
+                            }
+                            else if (gameSettings.CharacterManager.Stage >= 1 && gameSettings.CharacterManager.Stage < 66)
+                            {
+                                Console.WriteLine("The heroes have cleared the floor. They see two doors; which door are they going through?");
+                                Console.WriteLine(@"1 - The yellow door
+2 - The black door");
+                                string doorInput = Console.ReadLine();
+                                bool breakLoop = false;
+                                switch (doorInput)
+                                {
+                                    case "1":
+                                        ProgressStage(gameSettings.CharacterManager, gameSettings.AllyParty, new RandomParty(1));
+                                        Console.WriteLine("The heroes move on through the yellow door. They see some more enemies.");
+                                        breakLoop = true;
+                                        break;
+                                    case "2":
+                                        break;
+                                    default:
+                                        Console.Write("Invalid Input. ");
+                                        break;
+                                }
+                                if (breakLoop) break;
+                                Console.WriteLine("The heroes move on through the black door. They are met with SATAN, crouching on top of a mountain of skulls.");
+                                ProgressStage(gameSettings.CharacterManager, gameSettings.AllyParty, gameSettings.EnemyPartyThree);
+                                gameSettings.CharacterManager.SetToLastStage();
+                                break;
+                            }
+                            else Console.WriteLine("The heroes won and SATAN was defeated! The world remains safe for now.");
+                        }
+
+                        Environment.Exit(0); // The game is finished
+                    }
+
+                    DisplayStatus(gameSettings.CharacterManager, character);
+
+                    if (gameSettings.FriendlyController.IsSameSide(character))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        ControllerTurn(gameSettings.FriendlyController, gameSettings.CharacterManager, character, gameSettings.AllyParty);
+                    }
+                    if (gameSettings.EnemyController.IsSameSide(character))
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Thread.Sleep(1000);
+                        ControllerTurn(gameSettings.EnemyController, gameSettings.CharacterManager, character, gameSettings.ActiveEnemyParty);
+                    }
+
+                    foreach (Character deadCharacter in gameSettings.CharacterManager.GetCharacters())
+                    {
+                        gameSettings.CharacterManager.RemoveDeadCharacter(deadCharacter);
+                    }
+
+                    Console.WriteLine();
+                    Thread.Sleep(500);
+                }
+            }
+        }
+
+        private void ControllerTurn(PartyController partyController, CharacterManager characterManager, Character character, Party activeParty)
+        {
+            partyController.TakeTurn(characterManager, character, activeParty);
+        }
+
+        private void DisplayStatus(CharacterManager characterManager, Character character)
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"=========================================== STAGE {characterManager.Stage} ===========================================");
+            characterManager.ShowStatus(Side.Friendly);
+            Console.WriteLine("---------------------------------------------- VS ---------------------------------------------");
+            characterManager.ShowStatus(Side.Enemy);
+            Console.WriteLine("===============================================================================================");
+
+            Console.WriteLine($"It is {character}'s turn...");
+        }
+
+        private void ProgressStage(CharacterManager characterManager, Party allyParty, Party enemyPartyNum)
+        {
+            characterManager.IncreaseStage();
+            allyParty.CollectPotions(gameSettings.ActiveEnemyParty.PotionCount);
+            allyParty.CollectGears(gameSettings.ActiveEnemyParty.GetGears());
+            characterManager.LevelUpHero();
+            characterManager.AddCharactersInFront(enemyPartyNum.GetCharacters());
+            gameSettings.SetActiveEnemyParty(enemyPartyNum);
+        }
+    }
+}
---
+++ src/ConsoleRpg/GameSettings.cs
@@ -0,0 +1,103 @@
+﻿using ConsoleRpg.Characters;
+using ConsoleRpg.Gears;
+using ConsoleRpg.Controllers;
+using ConsoleRpg.Parties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRpg
+{
+    public class GameSettings
+    {
+        private string? playerName;
+        public CharacterManager CharacterManager { get; private set; }
+        public Party AllyParty { get; private set; }
+        public Party ActiveEnemyParty { get; private set; }
+        public Party EnemyPartyTwo { get; private set; }
+        public Party EnemyPartyThree { get; private set; }
+        public PartyController FriendlyController { get; private set; }
+        public PartyController EnemyController { get; private set; }
+        public GameSettings()
+        {
+            CreateInitialSettings();
+            CreatePartySettings();
+        }
+
+        private void CreateInitialSettings()
+        {
+            Console.Write("You are the hero destined to hunt down Satan deep inside the dungeon. What is your name? ");
+            playerName = Console.ReadLine();
+            if (string.IsNullOrEmpty(playerName)) playerName = "Unnamed";
+
+            Console.Write("Would you like the hero party as a human player or a computer? Input 1 for player and 2 for computer. ");
+            string? friendlyPartyChoice = Console.ReadLine();
+            Console.Write("Would you like the enemy party as a human player or a computer? Input 1 for player and 2 for computer. ");
+            string? enemyPartyChoice = Console.ReadLine();
+            Console.WriteLine();
+
+            FriendlyController = DecideController(Side.Friendly, friendlyPartyChoice);
+            EnemyController = DecideController(Side.Enemy, enemyPartyChoice);
+            Console.WriteLine();
+        }
+
+        private void CreatePartySettings()
+        {
+            Hero hero = new Hero(playerName);
+            Legolas legolas = new Legolas("Legolas");
+            Skeleton skeletonOne = new Skeleton("Tim the skeleton");
+            Skeleton skeletonTwo = new Skeleton("Jake the skeleton");
+            Skeleton skeletonThree = new Skeleton("Emma the skeleton");
+            Satan satan = new Satan("Satan");
+
+            AllyParty = new Party(3);
+            AllyParty.AddToParty(legolas);
+            AllyParty.AddToParty(hero);
+            hero.EquipGear(new Sword("Sword"));
+            Party enemyPartyOne = new Party(1);
+            enemyPartyOne.AddToParty(skeletonOne);
+            skeletonOne.EquipGear(new Dagger("Dagger"));
+            EnemyPartyTwo = new Party(1);
+            EnemyPartyTwo.AddToParty(skeletonTwo);
+            EnemyPartyTwo.AddToParty(skeletonThree);
+            EnemyPartyTwo.AddGear(new Dagger("Dagger"));
+            EnemyPartyTwo.AddGear(new Dagger("Dagger"));
+            EnemyPartyThree = new Party(1);
+            EnemyPartyThree.AddToParty(satan);
+            satan.EquipGear(new Trident("Hellforged Trident"));
+            ActiveEnemyParty = enemyPartyOne;
+
+            CharacterManager = new CharacterManager();
+            CharacterManager.AddCharacters(enemyPartyOne.GetCharacters());
+            CharacterManager.AddCharacters(AllyParty.GetCharacters());
+        }
+
+        private PartyController DecideController(Side side, string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    Console.WriteLine($"The {side} side will be played by a human player.");
+                    return new HumanController(side);
+                case "2":
+                    Console.WriteLine($"The {side} side will be played by a computer.");
+                    return new ComputerController(side);
+                default:
+                    Console.WriteLine($"Invalid Input. The {side} side will be defaulted to computer.");
+                    return new ComputerController(side);
+            }
+        }
+
+        public void SetActiveEnemyParty(Party party)
+        {
+            ActiveEnemyParty = party;
+        }
+    }
+    public enum Side
+    {
+        Friendly,
+        Enemy
+    }
+}
