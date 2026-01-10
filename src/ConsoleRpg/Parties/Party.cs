0a9e7239b4a7d61aa6727a177b7accadd19c71d1
Yuwol <juneyoung.jeong66@outlook.com>
2026-01-11 00:08:24 +1000

    Add XML comments to all Party classes

--- src/ConsoleRpg/Parties/Party.cs
+++ src/ConsoleRpg/Parties/Party.cs
@@ -9,6 +9,9 @@ using System.Xml.Linq;
 
 namespace ConsoleRpg.Parties
 {
+    /// <summary>
+    /// Represents a party of characters on the same side, managing shared items such as gear and potions.
+    /// </summary>
     public class Party
     {
         protected List<Character> party = new List<Character>();
@@ -20,28 +23,53 @@ namespace ConsoleRpg.Parties
             PotionCount = potionCount;
         }
 
+        /// <summary>
+        /// Adds a character to the party.
+        /// </summary>
+        /// <param name="character">The character to be added.</param>
         public void AddToParty(Character character)
         {
             party.Add(character);
         }
 
+        /// <summary>
+        /// Gets the characters in the party.
+        /// </summary>
+        /// <returns>A copy of the list of characters in the party.</returns>
         public List<Character> GetCharacters()
         {
             return new List<Character>(party);
         }
 
+        /// <summary>
+        /// Adds a specified number of potions to the party.
+        /// </summary>
+        /// <param name="potionsToAdd">The number of potions to add.</param>
         public void CollectPotions(int potionsToAdd)
         {
             PotionCount += potionsToAdd;
             if (potionsToAdd > 0) Console.WriteLine($"The hero party has collected {potionsToAdd} potion/s from the last battle.");
         }
 
+        /// <summary>
+        /// Adds specified gear/s to the party.
+        /// </summary>
+        /// <param name="gears">The gear/s to add.</param>
         public void CollectGears(List<Gear> gears)
         {
             this.gears.AddRange(gears);
             if (gears.Count > 0) Console.WriteLine($"The hero party has collected {gears.Count()} gear/s from the last battle.");
         }
 
+        /// <summary>
+        /// Consumes an item from the party's inventory and applies its effect to the specified character.
+        /// </summary>
+        /// <param name="item">The item to consume.</param>
+        /// <param name="character">The character the item is applied to.</param>
+        /// <remarks>
+        /// If the specified item is not available in the party inventory, no action is taken.
+        /// Currently, the only item type available for use is potions.
+        /// </remarks>
         public void UseItem(Item item, Character character)
         {
             if (item == Item.Potion)
@@ -59,26 +87,47 @@ namespace ConsoleRpg.Parties
             }
         }
 
+        /// <summary>
+        /// Gets the available gear in the party's inventory.
+        /// </summary>
+        /// <returns>A copy of the list of gears in the party's inventory.</returns>
         public List<Gear> GetGears()
         {
             return new List<Gear>(gears);
         }
 
+        /// <summary>
+        /// Gets the first available gear in the party's inventory.
+        /// </summary>
+        /// <returns>The first available gear in the party's inventory.</returns>
         public Gear GetFirstGear()
         {
             return gears[0];
         }
 
+        /// <summary>
+        /// Adds a gear to the party's inventory.
+        /// </summary>
+        /// <param name="gear">The gear to be added.</param>
         public void AddGear(Gear gear)
         {
             gears.Add(gear);
         }
 
+        /// <summary>
+        /// Removes a gear from the party's inventory.
+        /// </summary>
+        /// <param name="gear">The gear to be removed.</param>
+        /// <remarks>The gear specified must be in the party's inventory.</remarks>
         public void RemoveGear(Gear gear)
         {
             gears.Remove(gear);
         }
 
+        /// <summary>
+        /// Determines if the party's inventory has any gear.
+        /// </summary>
+        /// <returns>True if the party has any available gear; otherwise, false.</returns>
         public bool HasGear()
         {
             if (gears.Count > 0) return true;
--- src/ConsoleRpg/Parties/RandomParty.cs
+++ src/ConsoleRpg/Parties/RandomParty.cs
@@ -8,6 +8,10 @@ using System.Threading.Tasks;
 
 namespace ConsoleRpg.Parties
 {
+    /// <summary>
+    /// Represents a party of skeletons on the enemy side, managing shared items such as gear and potions.
+    /// </summary>
+    /// <remarks>Every time this party is created, it will hold one more skeleton than the last.</remarks>
     public class RandomParty : Party
     {
         static int enemyCount = 1;
